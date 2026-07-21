b4f237fa5a3cde061bca0b4a989a062f3aaa7e7d
Ofir Gal <ofirgal753@gmail.com>
2026-07-21 11:30:20 +0300

    support ws 10

--- statusbar/sketchybar/update-ws-cache.sh
+++ statusbar/sketchybar/update-ws-cache.sh
@@ -11,15 +11,15 @@ FOCUSED_GROUP="${FOCUSED_RAW%%[bc]}"
 
 # Single aerospace call, count windows per group in one awk pass
 aerospace list-windows --all --format '%{workspace}' 2>/dev/null | awk -v fg="$FOCUSED_GROUP" '
-BEGIN { for (i=1; i<=9; i++) count[i]=0 }
+BEGIN { for (i=1; i<=10; i++) count[i]=0 }
 {
     ws = $1
     gsub(/[bc]$/, "", ws)
-    if (ws+0 >= 1 && ws+0 <= 9) count[ws+0]++
+    if (ws+0 >= 1 && ws+0 <= 10) count[ws+0]++
 }
 END {
     print fg
-    for (i=1; i<=9; i++) {
+    for (i=1; i<=10; i++) {
         has = (count[i] > 0) ? 1 : 0
         print i, has, count[i]
     }
